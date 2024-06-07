fd10f7f4c4e331bc7df0ef41ece32870a10d10a4
Ayo <aogunkinle@lcs.on.ca>
2024-06-07 14:19:10 -0400

    Initial Commit

---
+++ LCSYelp/LCSYelpApp.swift
@@ -0,0 +1,17 @@
+//
+//  LCSYelpApp.swift
+//  LCSYelp
+//
+//  Created by Ayodeji Ogunkinle on 2024-06-07.
+//
+
+import SwiftUI
+
+@main
+struct LCSYelpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
