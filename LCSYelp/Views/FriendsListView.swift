34d061796c3cfd1d4ae3f3081541fae12a0946be
Ayo <aogunkinle@lcs.on.ca>
2024-06-12 11:32:10 -0400

    used abstrcation through helper view's

--- LCSYelp/Views/FriendsListView.swift
+++ LCSYelp/Views/FriendsListView.swift
@@ -7,22 +7,22 @@
 
 import SwiftUI
 
-struct FriendsListView: View {
-    
-    
-    // MARK: Stored properties
-    let friends: [Friend]
-    
-    // MARK: Computed properties
-    var body: some View {
-        VStack {
-            List(friends) { friend in
-                FriendView(friendToShow: friend)
+    struct FriendsListView: View {
+        
+        
+        // MARK: Stored properties
+        let friends: [Friend]
+        
+        // MARK: Computed properties
+        var body: some View {
+            VStack {
+                List(friends) { friend in
+                    FriendView(friendToShow: friend)
+                }
+                
             }
-            
         }
     }
-}
 
 #Preview {
     FriendsListView(friends: exampleFriends)
