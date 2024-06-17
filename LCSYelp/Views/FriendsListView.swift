7a6b7a46ee78ce64b02d4e97da1a604e9c3e177f
Ayo <aogunkinle@lcs.on.ca>
2024-06-17 15:06:24 -0400

    created a new property within the view

--- LCSYelp/Views/FriendsListView.swift
+++ LCSYelp/Views/FriendsListView.swift
@@ -14,10 +14,13 @@ struct FriendsListView: View {
     let friends: [Friend]
     @State private var searchText: String = ""
     
+    @State private var addFriendSheetIsShowing = false
+    
     
     // MARK: Computed properties
     var body: some View {
-        VStack {
+        NavigationStack{
+            VStack {
                 List(friends) { friend in
                     FriendView(friendToShow: friend)
                 }
@@ -25,44 +28,48 @@ struct FriendsListView: View {
             }
             .searchable(text: $searchText)
             .toolbar {
-                ToolbarItem(placement: .topBarLeading) {
+                ToolbarItem(placement: .topBarTrailing) {
                     Button {
                         // This will show the add friendView
                     } label: {
                         Image(systemName: "plus")
                     }
-
                 }
-            }
-        }
-    }
-    
-    // MARK: Functions
-    func filter(friends: [Friend], on providedText: String) -> [Friend] {
-        if providedText.isEmpty{
-            return friends
-        } else{
-            // Make empty array
-            var filteredfriends: [Friend] = []
-            
-            for friend in friends {
-                if friend.username.contains(providedText){
-                    filteredfriends.append(friend)
-                }
-            }
-                                
-                // Return the list of friends that contained the provided text
-                return filteredfriends
-            }
-            
                 
-            
-            
-            
+            }
+            .sheet(isPresented: $addFriendSheetIsShowing, content: {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Sheet Content")/*@END_MENU_TOKEN@*/
+            })
         }
-    
-    
-    #Preview {
-        FriendsListView(friends: exampleFriends)
     }
+}
+
+// MARK: Functions
+func filter(friends: [Friend], on providedText: String) -> [Friend] {
+    if providedText.isEmpty{
+        return friends
+    } else{
+        // Make empty array
+        var filteredfriends: [Friend] = []
+        
+        for friend in friends {
+            if friend.username.contains(providedText){
+                filteredfriends.append(friend)
+            }
+        }
+        
+        // Return the list of friends that contained the provided text
+        return filteredfriends
+    }
+    
+    
+    
+    
+    
+}
+
+
+#Preview {
+    FriendsListView(friends: exampleFriends)
+}
 
